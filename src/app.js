72239d614f929399ae3a894f1335b7d7fd9ca73c
Savio Dias Viana <saviodiasviana@gmail.com>
2023-04-07 10:33:59 -0300

    Paginação implementada - teste

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body
     console.log(req.body)
-    if (!username || !avatar||(typeof(username)!=="string")||(typeof(avatar)!=="string")) {
+    if (!username || !avatar || (typeof (username) !== "string") || (typeof (avatar) !== "string")) {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
     users.push({ username, avatar })
@@ -21,7 +21,7 @@ app.post("/sign-up", (req, res) => {
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body
     const user = users.find((u) => u.username === username)
-    if (!username || !tweet||(typeof(username)!=="string")||(typeof(tweet)!=="string")) {
+    if (!username || !tweet || (typeof (username) !== "string") || (typeof (tweet) !== "string")) {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
     if (!user) {
@@ -32,19 +32,25 @@ app.post("/tweets", (req, res) => {
     res.status(201).send("ok")
 })
 
-app.get("/tweets", (req, res)=>{
-    if(tweets.length>10){
-        const tenTweets = tweets.filter((t,i)=>i>=tweets.length-10?t:'')
-        res.send(tenTweets)
-    }else{
-        res.send(tweets)
+app.get("/tweets", (req, res) => {
+    const { page } = req.query
+
+    if (page === undefined) {
+        const tenTweets = tweets.filter((t, i) => i >= tweets.length - 10 ? t : '')
+        return res.send(tenTweets)
     }
+    if (Number(page) >= 1) {
+        const myTweets = tweets.filter((t, i) => (i >= tweets.length - (10*page)) && (i<tweets.length - (10*(page-1))) ? t : '')
+        return res.send(myTweets)
+    }
+    
 })
 
-app.get("/tweets/:username",(req,res)=>{
-    const {username} = req.params
-    res.send(tweets.filter((t)=>t.username===username)) 
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params
+    res.send(tweets.filter((t) => t.username === username))
 })
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`App funcionando na porta ${PORT}`))
